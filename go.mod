84b652a4f75a72425d40766c6568081d86aa2afa
vasu03 <vasurmakadia@gmail.com>
2024-07-12 19:33:44 +0530

    [ setup ] :: Initial setup.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vasu03/Go-Bank-API
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,9 @@
+// Declaring the main package
+package main
+
+import "fmt"
+
+// Defining the main() method
+func main()  {
+	fmt.Println("Hello");
+}
